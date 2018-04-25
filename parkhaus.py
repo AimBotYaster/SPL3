bafee03ecad030f7775660d4ab40820544c3db2e
AimBotYaster <yaser.tara22@gmail.com>
2018-04-25 10:25:17 +0200

    Fertiges Programm

--- parkhaus.py
+++ parkhaus.py
@@ -11,22 +11,30 @@ Haltstellen = int(haltestellen)
 
 for h in range (0,10):
 
-        einsteiger = input("Wie viele Personen steigen ein ")
-
-        Personen = Personen + int(einsteiger)
+        einsteiger = int(input("Wie viele Personen steigen ein? "))
+        aussteiger = int(input("Wie viele Personen steigen aus? "))
+        
 
         print("Es gibt ",haltestellen," Haltestellen")
 
         print("Es steigen " ,einsteiger, "Personen ein") 
 
-
-        if(int(Personen) > 60):
-            print ("Es dürfen maximal 60 Personen einsteigen")
+        print("Es steigen " ,aussteiger, "Personen aus") 
+        if(Personen+einsteiger > 60):
+            print ("Es können", Personen + einsteiger  - 60,"nicht einsteigen")
             Personen = 60
+            Personen=Personen-aussteiger
 
-        elif (int(Personen) < 0):
-            print("Es müssen mindestens 0 Personen im Bus sein")    
-
+        elif (Personen+einsteiger-aussteiger < 0):
+            print("Es müssen mindestens 0 Personen im Bus sein") 
+            Personen = 0
+            Personen = Personen + einsteiger
+        else:
+            Personen = Personen + einsteiger
+            Personen= Personen - aussteiger
+        
+            
+        print("Zurzeit sind ",Personen,"Personen im Bus")     
 
 
    
