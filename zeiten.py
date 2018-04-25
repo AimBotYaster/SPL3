fc4f1551b3e76ecec026f230cf49f5c18c4fc78f
AimBotYaster <yaser.tara22@gmail.com>
2018-04-25 11:14:43 +0200

    fertiges Programm

--- zeiten.py
+++ zeiten.py
@@ -1,29 +1,36 @@
 # zeiten.py 
 
+
 def zeitInSekunden(h,m,s):
-    gesamt = 0 
-    gesamt += h*3600
-    gesamt+= m*60
-    gesamt+=s
-    return gesamt 
+    beginnzeit = 0 
+    beginnzeit += h*3600
+    beginnzeit+= m*60
+    beginnzeit+= s
+    return beginnzeit
 
 
-beginnZeit = input("Beginnzeit:")
-# endeZeit = input ("Endezeit")
 
-#Zeit in Format HH:MM:SS
-
+beginnZeit = input("Beginnzeit: ")
+endeZeit = input ("Endezeit: ")
 beginn = beginnZeit.split(":")
-print (beginn)
+
+end = endeZeit.split(":")
+
+
 
 h = int(beginn[0])
 m = int(beginn[1])
 s =int(beginn[2])
 
-print("Stunden:",h)
-print("Minuten:",m)
-print("Sekunden:",s)
+h1 = int(end[0])
+m1 = int(end[1])
+s1 =int(end[2])
 
 
-beginnSekunden = zeitInSekunden (h,m,s)
-print("Gesamt Sekunden Beginn :" ,beginnSekunden)
+
+
+a = zeitInSekunden (h,m,s)
+e = zeitInSekunden (h1,m1,s1)
+Differenz = e-a
+
+print(Differenz)
